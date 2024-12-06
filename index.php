a2e85a5e1d5f8979a88b00756864650ea178e88f
edimonndi <albografifm@gmail.com>
2024-12-06 02:08:13 +0100

    Added LICENSE to index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Trading Journal
+ * Copyright (c) 2024 @edimonndi
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 // Redirect to journal.php
 header("Location: journal.php");
 exit();
